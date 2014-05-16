7939a8c15c6ae6cd448075512b74c29d90914531
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-05-16 19:36:38 +0300

    Added first test for add(player)

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -2,11 +2,25 @@ package com.adaptionsoft.games.trivia;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import com.adaptionsoft.games.uglytrivia.Game;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class SomeTest {
 
-	@Test
-	public void true_is_true() throws Exception {
-		assertTrue(false);
-	}
+    @Test
+    public void testWhenAddingPlayerItsNameAndNumberAreWritten(){
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(stream);
+        System.setOut(printStream);
+
+        Game game = new Game();
+        game.add("Some Player");
+
+        assertEquals("Some Player was added\n" +
+                "They are player number 1\n", stream.toString());
+    }
+
+
 }

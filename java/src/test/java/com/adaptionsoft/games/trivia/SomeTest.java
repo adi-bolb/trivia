9240ab705b91d0dbfbf9bcad4c23d35617b8895d
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-05-21 10:17:56 +0300

    Added first system test

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -1,12 +1,28 @@
 package com.adaptionsoft.games.trivia;
 
 import static org.junit.Assert.*;
+
+import com.adaptionsoft.games.uglytrivia.Game;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class SomeTest {
 
 	@Test
-	public void true_is_true() throws Exception {
-		assertTrue(false);
-	}
+    public void gameWasCorrectlyAnsweredWhenNotInPenaltyBoxWriteCorrectAnswerToConsole(){
+
+        Game game = new Game();
+        game.add("some player name");
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        game.wasCorrectlyAnswered();
+
+        assertEquals("Answer was corrent!!!!\n" +
+                "some player name now has 1 Gold Coins.\n", outputStream.toString());
+    }
 }

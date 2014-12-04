83973b5a36e5df552d2b6aa1c3f8f648f098d6f2
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-12-04 15:01:59 +0200

    Extracted interface Console from ConsoleImpl

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/ConsoleImpl.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/ConsoleImpl.java
@@ -3,8 +3,8 @@ package com.adaptionsoft.games.uglytrivia;
 /**
  * Created by adi on 12/4/14.
  */
-public class ConsoleImpl {
-    protected static void writeLine(String playerWinMessage) {
+public class ConsoleImpl implements Console {
+    public void writeLine(String playerWinMessage) {
         System.out.println(playerWinMessage);
     }
 }

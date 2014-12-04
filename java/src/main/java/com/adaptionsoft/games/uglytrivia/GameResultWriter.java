93bc8f4e757c421fda26240bec57aab38343b561
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-12-04 15:11:55 +0200

    Added class GameResultWriter

---
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/GameResultWriter.java
@@ -0,0 +1,11 @@
+package com.adaptionsoft.games.uglytrivia;
+
+/**
+ * Created by adi on 12/4/14.
+ */
+public class GameResultWriter implements Console {
+    @Override
+    public void writeLine(String message) {
+
+    }
+}

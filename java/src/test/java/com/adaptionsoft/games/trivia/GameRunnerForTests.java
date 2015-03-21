f5fd42f5879a6cbabc1cf460caf91669aac1dc5f
Adi Bolboaca <adi_bolb123@yahoo.com>
2015-03-21 12:54:17 +0200

    Created class GameRunnerForTests

---
+++ java/src/test/java/com/adaptionsoft/games/trivia/GameRunnerForTests.java
@@ -0,0 +1,9 @@
+package com.adaptionsoft.games.trivia;
+
+import com.adaptionsoft.games.trivia.runner.GameRunner;
+
+/**
+ * Created by adi on 3/21/15.
+ */
+public class GameRunnerForTests extends GameRunner {
+}
